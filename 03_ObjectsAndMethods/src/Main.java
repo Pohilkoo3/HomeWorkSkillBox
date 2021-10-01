16ef327e8d98732d78ea6154537ef044da2acba7
Oleg <pohilkoo3@rambler.ru>
2021-10-01 15:48:16 +0300

    Урок 3. Доработка метода Printer.

--- 03_ObjectsAndMethods/src/Main.java
+++ 03_ObjectsAndMethods/src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
 
     public static void main(String[] args) {
-        Basket basket = new Basket();
+        /*Basket basket = new Basket();
         basket.add("Milk", 40);
         basket.print("Milk");
         Basket olegBasket = new Basket(600);
@@ -16,33 +16,19 @@ public class Main {
         vasyua.summ();
         vasyua.mult();
         vasyua.findMax();
-        vasyua.findMin();
+        vasyua.findMin();*/
 
-        Printer olegPrint = new Printer();
-        olegPrint.append("About Volgograd", "Novel", 45);
-        olegPrint.append("Мой родной дом ... конец.","Рассказ.", 10);
-        olegPrint.getPendingPagesCount();
-        olegPrint.print();
-        olegPrint.getPendingPagesCount();
-       Printer.getTotalPages();
-
-       Printer yanaPrinter = new Printer();
-       yanaPrinter.append("Text. Text. Text.", "dsafljk;", 36);
-       yanaPrinter.getPendingPagesCount();
-       yanaPrinter.print();
-       Printer.getTotalPages();
-
-       Printer my = new Printer();
-       my.append("jhkljh",10);
-       my.print();
-        Printer.getTotalPages();
-
-
-
-
-
-
-
+        Printer printer1 = new Printer();
+        printer1.append("Обращение");
+        printer1.append("jk",10);
+        printer1.append("kjhkkjgkj",15,"Заява");
+        printer1.append("New");
+        printer1.print();
+        printer1.print();
+        printer1.append("заява для проверки");
+        printer1.print();
+        printer1.print();
+        printer1.getTotalPages();
 
     }
 }
--- 03_ObjectsAndMethods/src/Printer.java
+++ 03_ObjectsAndMethods/src/Printer.java
@@ -4,60 +4,50 @@ import java.util.SplittableRandom;
 
 public class Printer {
 
-    static int totalPages = 0;
+    private int totalPages = 0;
     private String queue = "";
-    int pendingPagesCount = 0;
+    private int pendingPagesCount;
 
-
-
-    public  Printer() {
-        System.out.println("New objekt print:");
-    }
 
     public void append(String text) { //в первом методе мы теперь, имеем возможность распечатать только текст
         append(text,1);
     }
 
     public void append(String text, int pages){ //во втором методе мы теперь, имеем возможность распечатать текст и
-        //определить количество страниц, одна страница будет по умолчанию (меньше ведь быть не может).
-        append(text, "",1 );
+        //определить количество страниц.
+        append(text, pages, "");
     }
 
-    public void append(String text, String name, int pages){ //здесь можем заполнить текст, имя документа и количество страниц
+    public void append(String text, int pages, String name){ //здесь можем заполнить текст, имя документа и количество страниц
         queue = queue + "\n" + text + " " + name + " - " + pages + " стр." ;
         pendingPagesCount = pendingPagesCount + pages;
     }
 
-    public void getPendingPagesCount(){
-        System.out.println("Распечатка для объекта " + pendingPagesCount + " стр.");
-    }
+        public int getPendingPagesCount(){
+        return pendingPagesCount;
+        }
 
-    public static void getTotalPages(){
-        System.out.println("Тест принтера. Распечатано " + totalPages + " стр.");
-    }
-
-
-    public void print(){
+    public void print(){ //метод распечатки, печатает отправленные страницы и очищает очередь печати.
         if (pendingPagesCount == 0){
             System.out.println("В очереди нет документов на распечатку.");
+            return;
         } else {
             System.out.println("В очереди следующие документы: " + queue);
         }
-
+        System.out.println("Распечатана " + getPendingPagesCount() + " стр.");
         totalPages = totalPages + pendingPagesCount;
+
         clear();
     }
 
+    public int getTotalPages(){ //Метод для вывода информации о количестве страниц, распечатанных принтером
+        System.out.printf("Всего принтером распечатано %d страниц.\n", totalPages);
+        return totalPages;
+    }
     public void clear(){
         queue = "";
         pendingPagesCount = 0;
 
     }
-
-
-
-
-
-
 
 }
