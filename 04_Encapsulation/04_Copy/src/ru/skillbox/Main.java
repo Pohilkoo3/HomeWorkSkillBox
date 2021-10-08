b2b568127cf05272e6dc0c4583161d022a4d3acc
Oleg <pohilkoo3@rambler.ru>
2021-10-08 21:33:36 +0300

    Модуль 4. Создание копии. Доработка.

--- 04_Encapsulation/04_Copy/src/ru/skillbox/Main.java
+++ 04_Encapsulation/04_Copy/src/ru/skillbox/Main.java
@@ -4,20 +4,21 @@ public class Main {
 
     public static void main(String[] args) {
         Dimensions dimensions = new Dimensions(15,25,35);
-        Cargo oleg = new Cargo("a12345678", dimensions,654.23,"volgograd", false,true);
+        Cargo oleg = new Cargo("a12345678", dimensions,654.23,"Москва", false,true);
         System.out.println(oleg);
 
-        Dimensions copyDimensions = new Dimensions(dimensions.getHeight(),500,dimensions.getWidth());//создаю копию Dimencions
-        // c измененной длиной груза
-        Cargo copyCargoOleg = new Cargo(oleg.getRegNumber(), copyDimensions,1200,"Москва",
-                oleg.isTwist(), oleg.isFragile()); // создаю копию с измененной массой, адресом доставки, и размерами.
+        Dimensions copyDimensions = new Dimensions(dimensions.getHeight(), dimensions.getLength(),
+                dimensions.getWidth());//создаю полную копию Dimencions
+        copyDimensions = copyDimensions.setHeight(100);
+        copyDimensions = copyDimensions.setLength(100);
+        copyDimensions = copyDimensions.setWidth(100);
+        Cargo copyCargoOleg = new Cargo(oleg.getRegNumber(), copyDimensions, oleg.getMass(), oleg.getAddress(),
+                oleg.isTwist(), oleg.isFragile());
+        copyCargoOleg = copyCargoOleg.setAddress("Волгоград");
+        copyCargoOleg = copyCargoOleg.setMass(1000.00);
         System.out.println(copyCargoOleg);
 
 
-
-
         System.out.println(oleg); //проверяю первый объект oleg. Он не изменился.
-
-
     }
 }
