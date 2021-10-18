967d9fe8f628d789ea8b2873431f102e54801ae0
Oleg <pohilkoo3@rambler.ru>
2021-10-18 21:47:38 +0300

    6.9 Домашняя работа. Сделано.

--- 06_PrimitiveDataTypes/IntOrInteger/src/Main.java
+++ 06_PrimitiveDataTypes/IntOrInteger/src/Main.java
@@ -1,12 +1,30 @@
 public class Main {
+
     public static void main(String[] args) {
         Container container = new Container();
         container.addCount(5672);
         System.out.println(container.getCount());
 
-        // TODO: ниже напишите код для выполнения задания:
-        //  С помощью цикла и преобразования чисел в символы найдите все коды
-        //  букв русского алфавита — заглавных и строчных, в том числе буквы Ё.
+        Main search = new Main();
+        System.out.println(search.getNumberChar('А'));
+        System.out.println(search.getNumberChar('я'));
+        System.out.println(search.getNumberChar('Ё'));
+        System.out.println(search.getNumberChar('ё'));
+
+        /*for (int i = 0; i <= 65535; i++) {//цикл для вывода символов и их номеров
+            char y = (char) i;
+            System.out.println(i + " - " + y);*/
 
     }
-}
+
+
+        public int getNumberChar(char symbol) {//сделал метод, который перебирает символы и возвращает номер необходимого
+            int i = 0;
+            for (; i < 65_535; i++) {
+                if ((char) i == symbol) {
+                    break;
+                }
+            }return i;
+        }
+    }
+
