77c89ae47b924b340fbd4c2747812910995ec45e
Oleg <pohilkoo3@rambler.ru>
2021-10-22 17:09:47 +0300

    7.10 Домашняя работа. Метка времени (timestamp). Задание 1. Выполнено.

--- 07_NumbersAndDates/Date/src/main/java/Main.java
+++ 07_NumbersAndDates/Date/src/main/java/Main.java
@@ -1,22 +1,26 @@
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
 
 public class Main {
-
+    public static final String LS = System.lineSeparator();
     public static void main(String[] args) {
-
         int day = 31;
         int month = 12;
         int year = 1990;
-
         System.out.println(collectBirthdays(year, month, day));
-
     }
 
     public static String collectBirthdays(int year, int month, int day) {
-
-        //TODO реализуйте метод для построения строки в следующем виде
-        //0 - 31.12.1990 - Mon
-        //1 - 31.12.1991 - Tue
-        
-        return "";
+        LocalDate birthday = LocalDate.of(year,month,day);
+        LocalDate now = LocalDate.now();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy - EEE").localizedBy(Locale.US);
+        String stringBirthday = "";
+        int i = 0;
+        while (birthday.isBefore(now.plusDays(1))){
+            stringBirthday = stringBirthday.concat(i + " - " + formatter.format(birthday) + LS);
+            i +=1;
+            birthday = birthday.plusYears(1);
+        } return stringBirthday;
     }
 }
