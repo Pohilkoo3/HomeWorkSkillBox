16414b46c347867daa35e67176fd8e08d2c33034
Oleg <pohilkoo3@rambler.ru>
2021-10-22 18:39:20 +0300

    7.10 Домашняя работа. Метка времени (timestamp). Задание 2.

--- 07_NumbersAndDates/Periods/src/main/java/Main.java
+++ 07_NumbersAndDates/Periods/src/main/java/Main.java
@@ -1,13 +1,17 @@
 import java.time.LocalDate;
+import java.time.Period;
 
 public class Main {
-
+  private static final LocalDate BIRTHDAY_JAVA = LocalDate.of(1995, 5,23);
   public static void main(String[] args) {
-
+    System.out.println(getPeriodFromBirthday(BIRTHDAY_JAVA));
   }
 
   private static String getPeriodFromBirthday(LocalDate birthday) {
-    return "";
-  }
-
+    Period period = Period.between(birthday, LocalDate.now());
+    int daysFromBirthday = period.getDays();
+    int monthsFromBirthday = period.getMonths();
+    int yearsFromBirthday = period.getYears();
+    return  yearsFromBirthday + " years, " + monthsFromBirthday + " months, " +  daysFromBirthday + " days";
+    }
 }
