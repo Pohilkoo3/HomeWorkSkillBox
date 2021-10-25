2475ee555baa3e402655256ecdaa992344e7717b
Oleg <pohilkoo3@rambler.ru>
2021-10-25 17:17:58 +0300

    Home work 8. Part 1. Done.

--- 08_Strings/TotalEarningsCalculator/src/main/java/Main.java
+++ 08_Strings/TotalEarningsCalculator/src/main/java/Main.java
@@ -3,7 +3,18 @@ public class Main {
   public static void main(String[] args) {
 
     String text = "Вася заработал 5000 рублей, Петя - 7563 рубля, а Маша - 30000 рублей";
-    //TODO: напишите ваш код, результат вывести в консоль
+    String search = " руб";
+    int stop = text.lastIndexOf(search);
+    int start = text.lastIndexOf(' ', stop-1);
+    String salaryMasha = text.substring(start+1,stop);
+    int stopVasyl = text.indexOf(search);
+    int startVasyl = text.lastIndexOf(' ',stopVasyl-1);
+    String salaryVasyl =  text.substring(startVasyl,stopVasyl);
+    int stopPetr = text.indexOf(search,stopVasyl+1);
+    int startPetr = text.lastIndexOf(' ', stopPetr-1);
+    String salaryPetr = text.substring(startPetr, stopPetr);
+    System.out.println(Integer.parseInt(salaryMasha.trim()) + Integer.parseInt(salaryPetr.trim())
+            + Integer.parseInt(salaryVasyl.trim()));
   }
 
 }
