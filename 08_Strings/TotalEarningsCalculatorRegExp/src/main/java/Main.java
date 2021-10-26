e7e9a12eece7721a70fe4c823edb4bd571b21573
Oleg <pohilkoo3@rambler.ru>
2021-10-26 17:42:00 +0300

    Модуль 8. Задание 3. Работа с регулярными выражениями. Сделано

--- 08_Strings/TotalEarningsCalculatorRegExp/src/main/java/Main.java
+++ 08_Strings/TotalEarningsCalculatorRegExp/src/main/java/Main.java
@@ -1,12 +1,24 @@
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Main {
 
   public static void main(String[] args) {
-
+    System.out.println(calculateSalarySum("Никто ничего не заработал"));
   }
 
-  public static int calculateSalarySum(String text){
-    //TODO: реализуйте метод
-    return -1;
-  }
+  public static int calculateSalarySum(String text) {
+    String regex = "[\\s]{1,2}[0-9]{2,7}[\\s]{1,2}";
+    Pattern pattern = Pattern.compile(regex);
+    Matcher matcher = pattern.matcher(text);
 
+    int totalSalary = 0;
+    while (matcher.find()) {
+      int start = matcher.start();
+      int end = matcher.end();
+      int salaryInt = Integer.parseInt((text.substring(start, end)).trim());
+      totalSalary += salaryInt;
+    }
+    return totalSalary;
+  }
 }
