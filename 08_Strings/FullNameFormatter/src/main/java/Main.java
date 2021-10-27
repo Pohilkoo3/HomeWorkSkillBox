b9c61fb908415db0f8746b8351442b4ce8908ff2
Oleg <pohilkoo3@rambler.ru>
2021-10-27 18:15:11 +0300

    Homework 8.2. done

--- 08_Strings/FullNameFormatter/src/main/java/Main.java
+++ 08_Strings/FullNameFormatter/src/main/java/Main.java
@@ -7,32 +7,33 @@ public class Main {
     Scanner scanner = new Scanner(System.in);
     while (true) {
       String input = scanner.nextLine();
-      for (int i = 0; i < input.length(); i++){
-       if (Character.isDigit(input.charAt(i))) {
-          if(input.charAt(i) != ' ' || input.charAt(i) != '-' ){
-              System.out.println("Введенная строка не является ФИО");
-              return;
-            }
+      for (int i = 0; i < input.length(); i++) {
+        if (!Character.UnicodeBlock.of(input.charAt(i)).
+                equals(Character.UnicodeBlock.CYRILLIC)) {
+          if (input.charAt(i) != '-' && input.charAt(i) != ' ') {
+            System.out.println("Введенная строка не является ФИО");
+            return;
+          }
         }
       }
-      int stopSurname = input.indexOf(" ");
-      int startMiddleName = input.lastIndexOf(' ');
-      if (stopSurname < 0 || startMiddleName < 0){
-        System.out.println("Введенная строка не является ФИО");
-        break;
+        int stopSurname = input.indexOf(" ");
+        int startMiddleName = input.lastIndexOf(' ');
+        if (stopSurname < 0 || startMiddleName < 0) {
+          System.out.println("Введенная строка не является ФИО");
+          return;
+        }
+        String surname = input.substring(0, stopSurname);
+        String middleName = input.substring(startMiddleName + 1);
+        String name = (input.substring(stopSurname, startMiddleName)).trim();
+        int searchNewSpaces = name.indexOf(' ');
+        if (name.isEmpty() || searchNewSpaces > 0) {
+          System.out.println("Введенная строка не является ФИО");
+          return;
+        }
+        System.out.println("Фамилия: " + surname);
+        System.out.println("Имя: " + name);
+        System.out.println("Отчество: " + middleName);
+break;
       }
-      String surname = input.substring(0,stopSurname);
-      String middleName = input.substring(startMiddleName + 1);
-      String name = (input.substring(stopSurname,startMiddleName)).trim();
-      int searchNewSpaces = name.indexOf(' ');
-      if (name.isEmpty() || searchNewSpaces > 0){
-        System.out.println("Введенная строка не является ФИО");
-        break;
-      }
-      System.out.println("Фамилия: " + surname);
-      System.out.println("Имя: " + name);
-      System.out.println("Отчество: " + middleName);
-      break;
     }
   }
-}
