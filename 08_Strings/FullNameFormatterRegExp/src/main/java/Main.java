453257f3430cd0aa140937948fdc37de12f7a1b1
Oleg <pohilkoo3@rambler.ru>
2021-10-28 18:01:38 +0300

    Модуль 8. Задание 5. Сделано

--- 08_Strings/FullNameFormatterRegExp/src/main/java/Main.java
+++ 08_Strings/FullNameFormatterRegExp/src/main/java/Main.java
@@ -1,18 +1,34 @@
 import java.util.Scanner;
+import java.util.regex.Pattern;
 
 public class Main {
-
+  private static final String ERROR = "Введенная строка не является ФИО";
   public static void main(String[] args) {
-
     Scanner scanner = new Scanner(System.in);
     while (true) {
       String input = scanner.nextLine();
       if (input.equals("0")) {
         break;
       }
-      //TODO:напишите ваш код тут, результат вывести в консоль.
-      //При невалидном ФИО вывести в консоль: Введенная строка не является ФИО
+      String regex = "[А-я\\s-]*";
+      if (!Pattern.matches(regex, input)){
+        System.out.println(ERROR);
+        return;
+      }
+      String[] words = input.split("\\s+");
+
+      if (words.length < 3) {
+        System.out.println(ERROR);
+        return;
+      }
+      System.out.println("Фамилия: " + words[0]);
+      System.out.println("Имя: " + words[1]);
+      System.out.println("Отчество: " + words[2]);
     }
   }
-
 }
+
+
+
+
+
