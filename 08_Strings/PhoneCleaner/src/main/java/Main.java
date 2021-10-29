1f32e27325f3788dfa4559039d2c809b6720395f
Oleg <pohilkoo3@rambler.ru>
2021-10-29 16:44:35 +0300

    Модуль 8. Задание 6. Сделано.

--- 08_Strings/PhoneCleaner/src/main/java/Main.java
+++ 08_Strings/PhoneCleaner/src/main/java/Main.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+import java.util.regex.Pattern;
 
 public class Main {
 
@@ -10,8 +11,16 @@ public class Main {
       if (input.equals("0")) {
         break;
       }
-      //TODO:напишите ваш код тут, результат вывести в консоль.
+      String clearNumber = input.replaceAll("[^0-9]","");
+      if (clearNumber.length() == 10){
+        clearNumber = "7" + clearNumber;
+      }
+      String regex = "[7,8][9][0-9]{8,9}";
+      if (!Pattern.matches(regex,clearNumber)) {
+        System.out.println("Неверный формат номера");
+        return;
+      }String rightNumber ="7".concat(clearNumber.substring(1));
+      System.out.println(rightNumber);
     }
   }
-
 }
