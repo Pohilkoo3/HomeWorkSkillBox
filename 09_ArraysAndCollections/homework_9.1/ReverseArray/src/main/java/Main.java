f4a6eae7bfa010ffc966eea5a6787f152029eedd
Oleg <pohilkoo3@rambler.ru>
2021-10-31 21:14:27 +0300

    Модуль 9. Задание 1. Исправленное.

--- 09_ArraysAndCollections/homework_9.1/ReverseArray/src/main/java/Main.java
+++ 09_ArraysAndCollections/homework_9.1/ReverseArray/src/main/java/Main.java
@@ -2,10 +2,5 @@ public class Main {
 
     public static void main(String[] args) {
         String line = "Каждый охотник желает знать, где сидит фазан";
-<<<<<<< HEAD
-
-
-=======
->>>>>>> f110056b24800c35d3a7bf0ea7d5366e45370773
     }
 }
--- 09_ArraysAndCollections/homework_9.1/ReverseArray/src/main/java/ReverseArray.java
+++ 09_ArraysAndCollections/homework_9.1/ReverseArray/src/main/java/ReverseArray.java
@@ -1,17 +1,11 @@
 public class ReverseArray {
 
-<<<<<<< HEAD
-
     public static String[] reverse (String[] strings){
         for (int i = 0; i < strings.length / 2; i++) {
             String temp = strings[i];
             strings[i] = strings[strings.length-1-i];
             strings[strings.length-1-i] = temp;
         }
-=======
-    //TODO: Напишите код, который меняет порядок расположения элементов внутри массива на обратный.
-    public static String[] reverse (String[] strings){
->>>>>>> f110056b24800c35d3a7bf0ea7d5366e45370773
         return strings;
     }
 }
