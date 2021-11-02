d2892f149e1ae35714ac1d1c4a92356d7173b69c
Oleg Pokhilko <pohilkoo3@rambler.ru>
2021-11-02 13:23:06 +0300

    Задание 9.1.3. Доработано

--- 09_ArraysAndCollections/homework_9.1/TwoDimensionalArray/src/main/java/TwoDimensionalArray.java
+++ 09_ArraysAndCollections/homework_9.1/TwoDimensionalArray/src/main/java/TwoDimensionalArray.java
@@ -11,11 +11,7 @@ public class TwoDimensionalArray {
         char[][] test = new char[size][size];
         for (int i = 0; i < test.length; i++){
             for (int j = 0; j < test[i].length; j++){
-                if (i == j || j == test[i].length-1-i){
-                    test[i][j] = 'X';
-                } else {
-                    test[i][j] = ' ';
-                }
+                test[i][j] = i == j || j == test[i].length-1-i ? 'X' : ' ';
             } System.out.println(test[i]);
         }
         return test;
