2de77b4c4d5f2df1014002e62f25458ed48fe8f8
Oleg Pokhilko <pohilkoo3@rambler.ru>
2021-11-03 16:52:48 +0300

    Задание 9.2. Домашняя работа сделана.

--- 09_ArraysAndCollections/homework_9.2/TodoList/src/main/java/TodoList.java
+++ 09_ArraysAndCollections/homework_9.2/TodoList/src/main/java/TodoList.java
@@ -2,28 +2,44 @@ import java.util.ArrayList;
 
 public class TodoList {
 
+    public ArrayList<String> todoList = new ArrayList<>();
+
     public void add(String todo) {
-        // TODO: добавьте переданное дело в конец списка
+        todoList.add(todo);
+        System.out.println("Добавлено дело \"" + todo + "\"");// TODO: добавьте переданное дело в конец списка
     }
 
     public void add(int index, String todo) {
-        // TODO: добавьте дело на указаный индекс,
-        //  проверьте возможность добавления
+        index = index > todoList.size() || index < 0 ? todoList.size() : index;     // TODO: добавьте дело на указаный индекс,
+        todoList.add( index, todo);
+        System.out.println("Добавлено дело \"" + todo + "\"");
     }
 
     public void edit(String todo, int index) {
+        if (index < 0 || index > todoList.size() - 1){          //  проверьте возможность изменения
+            return;
+        }
+        System.out.println("Дело \"" + todoList.get(index) + "\"" + " заменено на \"" + todo + "\"");
+        todoList.set(index,todo);
         // TODO: заменить дело на index переданным todo индекс,
-        //  проверьте возможность изменения
     }
 
     public void delete(int index) {
-        // TODO: удалить дело находящееся по переданному индексу,
-        //  проверьте возможность удаления дела
+        if (index < 0 || index > todoList.size() - 1){
+            System.out.println("Дело с таким номером не существует");//  проверьте возможность изменения
+            return;
+        }
+        System.out.println("Дело \"" + todoList.get(index) + "\" удалено.");
+        todoList.remove(index);                    // TODO: удалить дело находящееся по переданному индексу,                                               //  проверьте возможность удаления дела
     }
 
     public ArrayList<String> getTodos() {
-        // TODO: вернуть список дел
-        return new ArrayList<>();
+        return todoList;   // TODO: вернуть список дел
     }
-
+    public void list(){
+        for (int i = 0; i < todoList.size(); i++){
+            System.out.println(i + " - " + todoList.get(i));
+        }
+        System.out.println("\n");
+    }
 }
