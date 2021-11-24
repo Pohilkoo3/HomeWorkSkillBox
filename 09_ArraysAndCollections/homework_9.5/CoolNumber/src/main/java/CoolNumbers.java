f056e2fd666f2bb9dd5c4c10d7965ffc1f2d633d
Oleg Pokhilko <pohilkoo3@rambler.ru>
2021-11-24 16:34:23 +0300

    Задание 10.5 Сделано.

--- 09_ArraysAndCollections/homework_9.5/CoolNumber/src/main/java/CoolNumbers.java
+++ 09_ArraysAndCollections/homework_9.5/CoolNumber/src/main/java/CoolNumbers.java
@@ -1,29 +1,44 @@
-import java.util.Collections;
-import java.util.HashSet;
-import java.util.List;
-import java.util.TreeSet;
+import java.util.*;
 
 public class CoolNumbers {
 
     public static List<String> generateCoolNumbers() {
-        return Collections.emptyList();
+        String[] letters = {"А", "В", "Е", "К", "М", "Н", "О", "Р", "С", "Т", "У", "Х"};
+        ArrayList<String> result = new ArrayList<>();
+        for (int i = 0; i < letters.length; i++) {
+            for (int j = 0; j < 10; j++) {
+                for (int k = 0; k < 10; k++) {
+                    for (int l = 0; l < 10; l++) {
+                        for (int m = 0; m < letters.length; m++) {
+                            for (int n = 0; n < letters.length; n++) {
+                                for (int o = 10; o <= 199 ; o++) {
+                                    if (j == k && k == l && j != 0){
+                                        String resultNumber = letters[i] + j + k + l + letters[m] +letters[n] + o;
+                                        result.add(resultNumber);
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        return result;
     }
 
     public static boolean bruteForceSearchInList(List<String> list, String number) {
-        return false;
+        return list.contains(number);
     }
 
     public static boolean binarySearchInList(List<String> sortedList, String number) {
-        return false;
+      return Collections.binarySearch(sortedList, number) >= 0;
     }
 
-
     public static boolean searchInHashSet(HashSet<String> hashSet, String number) {
-        return false;
+        return hashSet.contains(number);
     }
 
     public static boolean searchInTreeSet(TreeSet<String> treeSet, String number) {
-        return false;
+        return treeSet.contains(number);
     }
-
 }
--- 09_ArraysAndCollections/homework_9.5/CoolNumber/src/main/java/Main.java
+++ 09_ArraysAndCollections/homework_9.5/CoolNumber/src/main/java/Main.java
@@ -1,12 +1,70 @@
+import java.math.BigInteger;
+import java.util.*;
+
 public class Main {
-    /*
+
+    public static void main(String[] args) {
+        List<String> test = CoolNumbers.generateCoolNumbers();
+
+        BigInteger start = new BigInteger(String.valueOf(System.currentTimeMillis()));
+        BigInteger startHC = start.multiply(new BigInteger("1000000"));//Х999ХХ199
+        BigInteger timeProcess = new BigInteger("0");
+        if (CoolNumbers.bruteForceSearchInList(test, "Х999ХХ199")) {
+            BigInteger stop = new BigInteger(String.valueOf(System.currentTimeMillis()));
+            BigInteger stopHC = stop.multiply(new BigInteger("1000000"));
+            timeProcess = stopHC.subtract(startHC);
+            System.out.println("Поиск перебором: номер найден, поиск занял " + timeProcess + " наносекунд");
+        } else {
+            System.out.println("Поиск перебором: номер не найден, поиск занял " + timeProcess + " наносекунд");
+        }
+
+        Collections.sort(test);
+        BigInteger start1 = new BigInteger(String.valueOf(System.currentTimeMillis()));
+        BigInteger startHC1 = start1.multiply(new BigInteger("1000000"));
+        BigInteger timeProcess1 = new BigInteger("0");
+        if (CoolNumbers.binarySearchInList(test, "Х999ХХ199")) {
+            BigInteger stop1 = new BigInteger(String.valueOf(System.currentTimeMillis()));
+            BigInteger stopHC1 = stop1.multiply(new BigInteger("1000000"));
+            timeProcess1 = stopHC1.subtract(startHC1);
+            System.out.println("Бинарный поиск: номер найден, поиск занял "  + timeProcess1 + " наносекунд");
+        }else {
+            System.out.println("Бинарный поиск: номер не найден, поиск занял " + timeProcess1 + " наносекунд");
+        }
+
+        HashSet<String> copyHash = new HashSet<>(test);
+        BigInteger start2 = new BigInteger(String.valueOf(System.currentTimeMillis()));
+        BigInteger startHC2 = start2.multiply(new BigInteger("1000000"));
+        BigInteger timeProcess2 = new BigInteger("0");
+        if (CoolNumbers.searchInHashSet(copyHash, "Х999ХХ199")) {
+            BigInteger stop2 = new BigInteger(String.valueOf(System.currentTimeMillis()));
+            BigInteger stopHC2 = stop2.multiply(new BigInteger("1000000"));
+            timeProcess2 = stopHC2.subtract(startHC2);
+            System.out.println("Поиск в HashSet: номер найден, поиск занял "  + timeProcess2 + " наносекунд");
+        }else {
+            System.out.println("Поиск в HashSet: номер не найден, поиск занял " + timeProcess2 + " наносекунд");
+        }
+
+        TreeSet<String> copyTreeSet = new TreeSet<>(test);
+        BigInteger start3 = new BigInteger(String.valueOf(System.currentTimeMillis()));
+        BigInteger startHC3 = start3.multiply(new BigInteger("1000000"));
+        BigInteger timeProcess3 = new BigInteger("0");
+        if (CoolNumbers.searchInTreeSet(copyTreeSet, "Х999ХХ199")) {
+            BigInteger stop3 = new BigInteger(String.valueOf(System.currentTimeMillis()));
+            BigInteger stopHC3 = stop3.multiply(new BigInteger("1000000"));
+            timeProcess3 = stopHC3.subtract(startHC3);
+            System.out.println("Поиск в TreeSet: номер найден, поиск занял "  + timeProcess3 + " наносекунд");
+        }else {
+            System.out.println("Поиск в TreeSet: номер не найден, поиск занял " + timeProcess3 + " наносекунд");
+        }
+
+
+
+
+    }
+}
+   /*
     TODO:
      - реализовать методы класса CoolNumbers
      - посчитать время поиска введимого номера в консоль в каждой из структуры данных
      - проанализоровать полученные данные
      */
-
-    public static void main(String[] args) {
-
-    }
-}
