d140035116fbdb8f3aa22f607db68de978a56947
Oleg Pokhilko <pohilkoo3@rambler.ru>
2021-11-26 13:20:49 +0300

    Задание 10.5 Доработано.

--- 09_ArraysAndCollections/homework_9.5/CoolNumber/src/main/java/Main.java
+++ 09_ArraysAndCollections/homework_9.5/CoolNumber/src/main/java/Main.java
@@ -1,70 +1,43 @@
-import java.math.BigInteger;
+
 import java.util.*;
 
 public class Main {
+    public static void main (String[]args){
 
-    public static void main(String[] args) {
-        List<String> test = CoolNumbers.generateCoolNumbers();
+        for (;;) {
+            System.out.println("Введите искомый номер. Для прекращения поиска введите команду END");
+            String search = new Scanner(System.in).nextLine();
+            if (search.equals("END")){
+                return;
+            }
 
-        BigInteger start = new BigInteger(String.valueOf(System.currentTimeMillis()));
-        BigInteger startHC = start.multiply(new BigInteger("1000000"));//Х999ХХ199
-        BigInteger timeProcess = new BigInteger("0");
-        if (CoolNumbers.bruteForceSearchInList(test, "Х999ХХ199")) {
-            BigInteger stop = new BigInteger(String.valueOf(System.currentTimeMillis()));
-            BigInteger stopHC = stop.multiply(new BigInteger("1000000"));
-            timeProcess = stopHC.subtract(startHC);
-            System.out.println("Поиск перебором: номер найден, поиск занял " + timeProcess + " наносекунд");
-        } else {
-            System.out.println("Поиск перебором: номер не найден, поиск занял " + timeProcess + " наносекунд");
+            List<String> test = CoolNumbers.generateCoolNumbers();
+            long start = System.nanoTime();
+            String result = CoolNumbers.bruteForceSearchInList(test, search) ? "" : "не ";
+            System.out.println("Поиск перебором: номер " + result + "найден, поиск занял " + (System.nanoTime() - start) +
+                    " наносекунд");
+
+            List<String> test1 = CoolNumbers.generateCoolNumbers();
+            Collections.sort(test1);
+            long start1 = System.nanoTime();
+            String result1 = CoolNumbers.binarySearchInList(test1, search) ? "" : "не ";
+            System.out.println("Бинарный поиск: номер " + result1 + "найден, поиск занял " + (System.nanoTime() - start1) +
+                    " наносекунд");
+
+            List<String> test2 = CoolNumbers.generateCoolNumbers();
+            HashSet<String> copyHash = new HashSet<>(test2);
+            long start2 = System.nanoTime();
+            String result2 = CoolNumbers.searchInHashSet(copyHash, search) ? "" : "не ";
+            System.out.println("Поиск в HashSet: номер " + result2 + "найден, поиск занял " + (System.nanoTime() - start2) +
+                    " наносекунд");
+
+            List<String> test3 = CoolNumbers.generateCoolNumbers();
+            TreeSet<String> copyTreeSet = new TreeSet<>(test3);
+            long start3 = System.nanoTime();
+            String result3 = CoolNumbers.searchInTreeSet(copyTreeSet, search) ? "" : "не ";
+            System.out.println("Поиск в TreeSet: номер " + result3 + "найден, поиск занял " + (System.nanoTime() - start3) +
+                    " наносекунд");
         }
-
-        Collections.sort(test);
-        BigInteger start1 = new BigInteger(String.valueOf(System.currentTimeMillis()));
-        BigInteger startHC1 = start1.multiply(new BigInteger("1000000"));
-        BigInteger timeProcess1 = new BigInteger("0");
-        if (CoolNumbers.binarySearchInList(test, "Х999ХХ199")) {
-            BigInteger stop1 = new BigInteger(String.valueOf(System.currentTimeMillis()));
-            BigInteger stopHC1 = stop1.multiply(new BigInteger("1000000"));
-            timeProcess1 = stopHC1.subtract(startHC1);
-            System.out.println("Бинарный поиск: номер найден, поиск занял "  + timeProcess1 + " наносекунд");
-        }else {
-            System.out.println("Бинарный поиск: номер не найден, поиск занял " + timeProcess1 + " наносекунд");
-        }
-
-        HashSet<String> copyHash = new HashSet<>(test);
-        BigInteger start2 = new BigInteger(String.valueOf(System.currentTimeMillis()));
-        BigInteger startHC2 = start2.multiply(new BigInteger("1000000"));
-        BigInteger timeProcess2 = new BigInteger("0");
-        if (CoolNumbers.searchInHashSet(copyHash, "Х999ХХ199")) {
-            BigInteger stop2 = new BigInteger(String.valueOf(System.currentTimeMillis()));
-            BigInteger stopHC2 = stop2.multiply(new BigInteger("1000000"));
-            timeProcess2 = stopHC2.subtract(startHC2);
-            System.out.println("Поиск в HashSet: номер найден, поиск занял "  + timeProcess2 + " наносекунд");
-        }else {
-            System.out.println("Поиск в HashSet: номер не найден, поиск занял " + timeProcess2 + " наносекунд");
-        }
-
-        TreeSet<String> copyTreeSet = new TreeSet<>(test);
-        BigInteger start3 = new BigInteger(String.valueOf(System.currentTimeMillis()));
-        BigInteger startHC3 = start3.multiply(new BigInteger("1000000"));
-        BigInteger timeProcess3 = new BigInteger("0");
-        if (CoolNumbers.searchInTreeSet(copyTreeSet, "Х999ХХ199")) {
-            BigInteger stop3 = new BigInteger(String.valueOf(System.currentTimeMillis()));
-            BigInteger stopHC3 = stop3.multiply(new BigInteger("1000000"));
-            timeProcess3 = stopHC3.subtract(startHC3);
-            System.out.println("Поиск в TreeSet: номер найден, поиск занял "  + timeProcess3 + " наносекунд");
-        }else {
-            System.out.println("Поиск в TreeSet: номер не найден, поиск занял " + timeProcess3 + " наносекунд");
-        }
-
-
-
-
     }
 }
-   /*
-    TODO:
-     - реализовать методы класса CoolNumbers
-     - посчитать время поиска введимого номера в консоль в каждой из структуры данных
-     - проанализоровать полученные данные
-     */
+
