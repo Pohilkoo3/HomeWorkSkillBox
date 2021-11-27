17c272f5942009604e318a81778b912d52eb31f4
Oleg Pokhilko <pohilkoo3@rambler.ru>
2021-11-27 10:59:08 +0300

    Задание 10.5 Доработано.

--- 09_ArraysAndCollections/homework_9.5/CoolNumber/src/main/java/Main.java
+++ 09_ArraysAndCollections/homework_9.5/CoolNumber/src/main/java/Main.java
@@ -3,6 +3,7 @@ import java.util.*;
 
 public class Main {
     public static void main (String[]args){
+        List<String> test = CoolNumbers.generateCoolNumbers();
 
         for (;;) {
             System.out.println("Введите искомый номер. Для прекращения поиска введите команду END");
@@ -10,29 +11,24 @@ public class Main {
             if (search.equals("END")){
                 return;
             }
-
-            List<String> test = CoolNumbers.generateCoolNumbers();
             long start = System.nanoTime();
             String result = CoolNumbers.bruteForceSearchInList(test, search) ? "" : "не ";
             System.out.println("Поиск перебором: номер " + result + "найден, поиск занял " + (System.nanoTime() - start) +
                     " наносекунд");
 
-            List<String> test1 = CoolNumbers.generateCoolNumbers();
-            Collections.sort(test1);
+            Collections.sort(test);
             long start1 = System.nanoTime();
-            String result1 = CoolNumbers.binarySearchInList(test1, search) ? "" : "не ";
+            String result1 = CoolNumbers.binarySearchInList(test, search) ? "" : "не ";
             System.out.println("Бинарный поиск: номер " + result1 + "найден, поиск занял " + (System.nanoTime() - start1) +
                     " наносекунд");
 
-            List<String> test2 = CoolNumbers.generateCoolNumbers();
-            HashSet<String> copyHash = new HashSet<>(test2);
+            HashSet<String> copyHash = new HashSet<>(test);
             long start2 = System.nanoTime();
             String result2 = CoolNumbers.searchInHashSet(copyHash, search) ? "" : "не ";
             System.out.println("Поиск в HashSet: номер " + result2 + "найден, поиск занял " + (System.nanoTime() - start2) +
                     " наносекунд");
 
-            List<String> test3 = CoolNumbers.generateCoolNumbers();
-            TreeSet<String> copyTreeSet = new TreeSet<>(test3);
+            TreeSet<String> copyTreeSet = new TreeSet<>(test);
             long start3 = System.nanoTime();
             String result3 = CoolNumbers.searchInTreeSet(copyTreeSet, search) ? "" : "не ";
             System.out.println("Поиск в TreeSet: номер " + result3 + "найден, поиск занял " + (System.nanoTime() - start3) +
