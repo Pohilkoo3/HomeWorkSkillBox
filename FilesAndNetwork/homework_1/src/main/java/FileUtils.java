3aaf1cd370e635e6d999b2fcdb7e729c3efabef5
Oleg Pokhilko <pohilkoo3@rambler.ru>
2022-01-13 17:14:30 +0300

    15.5 Домашняя работа №1. Done.

--- FilesAndNetwork/homework_1/src/main/java/FileUtils.java
+++ FilesAndNetwork/homework_1/src/main/java/FileUtils.java
@@ -1,6 +1,29 @@
+import java.io.File;
+
+
 public class FileUtils {
 
+    public static boolean getRightPath(String path){
+        File folder = new File(path);
+        if (!folder.exists() || !folder.isDirectory()){
+            throw new IllegalArgumentException("Name of file or path to the file is specified wrong.");
+        }return true;
+    }
+
     public static long calculateFolderSize(String path) {
-        return 0;
+        File folder = new File(path);
+        if (folder.isFile()){
+            return folder.length();
+        }
+        long sum = 0;
+        File[] files = folder.listFiles();
+        try {
+            for (File element : files) {
+                sum += calculateFolderSize(element.getPath());
+            }
+        } catch (Exception ex){
+            ex.printStackTrace();
+        }
+        return sum;
     }
 }
--- FilesAndNetwork/homework_1/src/main/java/Main.java
+++ FilesAndNetwork/homework_1/src/main/java/Main.java
@@ -1,5 +1,26 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
 
+        System.out.println("Введите путь до папки.");
+        String path = new Scanner(System.in).nextLine();
+        if (FileUtils.getRightPath(path)) {
+            System.out.println("Размер папки " + path + " составляет " +
+                    getHumanReadableSize(FileUtils.calculateFolderSize(path)) + ".");
+        }
     }
+
+    public static String getHumanReadableSize (long size){
+        double result = size;
+        int level = 0;
+        while (result > 999){
+            result = result / 1024;
+            level += 1;
+        }
+        String[] sizeName = {"B", "Kb", "Mb", "Gb", "Tb"};
+        return String.format("%.2f",result) + sizeName[level];
+    }
+
+
 }
