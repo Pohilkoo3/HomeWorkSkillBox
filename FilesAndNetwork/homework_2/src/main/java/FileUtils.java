cae6aa5c06f9afdef55187f45dd86f16a578c8a6
Oleg Pokhilko <pohilkoo3@rambler.ru>
2022-01-15 12:53:47 +0300

    15.7 Домашняя работа №2. Сделана.

--- FilesAndNetwork/homework_2/src/main/java/FileUtils.java
+++ FilesAndNetwork/homework_2/src/main/java/FileUtils.java
@@ -1,5 +1,28 @@
+import java.io.File;
+import java.nio.file.Files;
+
 public class FileUtils {
     public static void copyFolder(String sourceDirectory, String destinationDirectory) {
-        // TODO: write code copy content of sourceDirectory to destinationDirectory
+        File sources = new File(sourceDirectory);
+        File destination = new File(destinationDirectory);
+        if (!sources.exists()) {
+            throw new IllegalArgumentException("Запрошенный путь не существует.");
+        }
+        if (sources.isDirectory()) {
+            if (!destination.exists()) {
+                destination.mkdir();
+            }
+            File[] files = sources.listFiles();
+            for (File element : files) {
+                String pathDestination = destination.getAbsolutePath() + "\\" + element.getName();
+                copyFolder(element.getAbsolutePath(), pathDestination);
+            }
+        }else {
+            try {
+                Files.copy(sources.toPath(), destination.toPath());
+            } catch (Exception ex) {
+                ex.printStackTrace();
+            }
+        }
     }
 }
--- FilesAndNetwork/homework_2/src/main/java/Main.java
+++ FilesAndNetwork/homework_2/src/main/java/Main.java
@@ -1,4 +1,21 @@
-public class Main {
-    public static void main(String[] args) {
+import java.util.Scanner;
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите путь к папке которую необходимо копировать.");
+        String testSources = scanner.nextLine();
+        System.out.println("Введите путь к папке в которую необходимо провести копирование.");
+        String testDestination = scanner.nextLine();
+
+        try {
+            FileUtils.copyFolder(testSources, testDestination);
+            System.out.println("Копирование успешно завершено.");
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
     }
 }
+
