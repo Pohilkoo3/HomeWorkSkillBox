6d52ed6c178e027ab0a1c59563d1232a49eea98b
Oleg Pokhilko <pohilkoo3@rambler.ru>
2022-01-31 15:52:54 +0300

    Модуль 16. Работа 1. Поиск среднего ежемесячного количества покупок курсов. SQL.

---
+++ SQLAndHibernate/JDBC/src/main/java/Main.java
@@ -0,0 +1,36 @@
+import java.sql.*;
+
+public class Main {
+    public static void main(String[] args) {
+        String url = "jdbc:mysql://localhost:3306/skillbox";
+        String user = "root";
+        String pass = "Oleg09061979$";
+
+        try {
+            Connection connection = DriverManager.getConnection(url, user, pass);
+            Statement statement = connection.createStatement();
+            ResultSet resultSet = getAveragePurchase(statement);
+            printResultSet(resultSet);
+            resultSet.close();
+            statement.close();
+            connection.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    static ResultSet getAveragePurchase (Statement statement) throws SQLException {
+        return statement.executeQuery("SELECT course_name, COUNT(*) as amount_purchase," +
+                "MAX(MONTH(subscription_date)) as amount_Months, " +
+                "COUNT(*)/MAX(MONTH(subscription_date)) as Average_purchase_per_Month" +
+                " FROM purchaselist GROUP BY(course_name)");
+    }
+
+    static void printResultSet(ResultSet resultSet) throws SQLException {
+        while (resultSet.next()){
+            String print = resultSet.getString("course_name") + " → " +
+                    (resultSet.getString("Average_purchase_per_Month")).substring(0,4);
+            System.out.println(print);
+        }
+    }
+}
