7b9ada637e6bf2a9221b21694dc5a2b6162b929c
Oleg Pokhilko <pohilkoo3@rambler.ru>
2022-03-04 11:10:49 +0300

    Модуль 19. Задание 4.

---
+++ SpringBootWebDevelopment/Service/src/main/java/main/DefaultController.java
@@ -0,0 +1,30 @@
+package main;
+
+import main.model.Task;
+import main.model.TaskRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Controller
+public class DefaultController {
+    @Autowired
+    TaskRepository taskRepository;
+
+@RequestMapping("/")
+    public String index(Model model){
+    Iterable<Task> iterable = taskRepository.findAll();
+    List<Task> taskList = new ArrayList<>();
+    for (Task element : iterable) {
+        taskList.add(element);
+    }
+    model.addAttribute("task", taskList);
+    model.addAttribute("tasksCount", taskList.size());
+    return "index";
+    }
+}
---
+++ SpringBootWebDevelopment/Service/src/main/resources/static/js/task.js
@@ -0,0 +1,51 @@
+$(function(){
+
+//$.get('/tasks/',{}, function(response){
+//        if(response.length == 0){
+//           alert('Все cупер. Все дела переделаны!');
+//        }
+//        for(i in response)
+//        {
+//            let element = '<div class="task">' + response[i].id  + ' → '+ response[i].textTask + '</div>';
+//            $('.mapping-all-tasks').append(element);
+//        }
+//        });
+
+$('.add-task').on('click', function()
+    {
+  let textTask = $('.add-text').val();
+    $.post('/tasks/', {textTask: textTask}, function(response){
+ if(response > 0){
+        $('.add-text').val('');
+        $('.all-tasks').html('У вас все получилось. Номер нового дела => ' + response);
+        }
+    });
+});
+
+ $('.getAllTasks').on('click', function()
+    {
+    $('.all-tasks').html('');
+$.get('/tasks/',{}, function(response){
+        if(response.length == 0){
+           alert('Все cупер. Все дела переделаны!');
+        }
+        for(i in response)
+        {
+            let element = response[i].id  + " → "+ response[i].textTask + " ;\n ";
+            $('.all-tasks').append(element);
+        }
+        });
+    });
+
+    $('.DeleteAllTasks').on('click', function()
+        {
+       $.ajax({
+           method: "DELETE",
+           url: "/tasks/",
+           success: true
+       });
+       $('.all-tasks').html('Прекрасно, все дела сделаны.');
+    });
+
+});
+
